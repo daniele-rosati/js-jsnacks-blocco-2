0b421ebed8d08d6f1c3325600775cece17ce991e
daniele-rosati <drosati151@gmail.com>
2024-03-12 16:33:32 +0100

    2 parole 2 prompt

---
+++ 2 parole 2 prompt/script.js
@@ -0,0 +1,22 @@
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// chiediamo all'utente di inserire 2 parole 
+
+const firstWord = prompt("Inserisci la prima parola");
+const secondWord = prompt("Inserisci la seconda parola");
+
+// determiniamo la lunghezza delle parole
+
+const lunghezzafirstWord = firstWord.length;
+const lunghezzasecondWord = secondWord.length;
+
+// stampiamo prima la parola più corta poi quella più lunga
+
+if (lunghezzafirstWord < lunghezzasecondWord){
+    console.log(firstWord);
+    console.log(secondWord);
+} else if (lunghezzasecondWord < lunghezzafirstWord) {
+    console.log(secondWord);
+    console.log(firstWord);
+}
